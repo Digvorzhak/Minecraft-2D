4b5d5383ceeabf40878790c3538e0261c8688e63
Digvorzhak <shon.h777@gmail.com>
2023-02-03 00:44:10 +0200

    progress

---
+++ game.js
@@ -0,0 +1,72 @@
+const world = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 2
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 3
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 4
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 6
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
+  [0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 8
+  [0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 9
+  [0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 10
+  [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 11
+  [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 12
+  [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 13
+  [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 14
+  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], // 15
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], // 16
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], // 17
+  [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], // 18
+  [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], // 19
+];
+
+const inventory = [];
+const board = document.getElementById("game-board");
+const shovel = document.querySelector(".shovel-btn");
+const axe = document.querySelector(".axe-btn");
+const pickaxe = document.querySelector(".pickaxe-btn");
+console.log(shovel, axe, pickaxe);
+
+function draw() {
+  for (let i = 0; i < world.length; i++) {
+    for (let j = 0; j < world[i].length; j++) {
+      if (world[i][j] === 0) {
+        world[i][j] = document.createElement("div");
+
+        board.appendChild(world[i][j]);
+      }
+      if (world[i][j] === 1) {
+        world[i][j] = document.createElement("div");
+        world[i][j].classList.add("nethergoldore");
+        board.appendChild(world[i][j]);
+      }
+      if (world[i][j] === 2) {
+        world[i][j] = document.createElement("div");
+        world[i][j].classList.add("dirthblock");
+        board.appendChild(world[i][j]);
+      }
+      if (world[i][j] === 3) {
+        world[i][j] = document.createElement("div");
+        world[i][j].classList.add("grassblock");
+        board.appendChild(world[i][j]);
+      }
+      if (world[i][j] === 4) {
+        world[i][j] = document.createElement("div");
+        world[i][j].classList.add("wood");
+        board.appendChild(world[i][j]);
+      }
+      if (world[i][j] === 5) {
+        world[i][j] = document.createElement("div");
+        world[i][j].classList.add("plant");
+        board.appendChild(world[i][j]);
+      }
+    }
+  }
+}
+
+draw();
+
+function shovelTool() {}
+
+shovel.addEventListener("click", shovelTool);
